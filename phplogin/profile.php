3c2631ea69176d81766b3c961fc52289d4c515a9
Gyonn03 <gyonkosters@gmail.com>
2021-04-15 15:33:49 +0200

    e

--- phplogin/profile.php
+++ phplogin/profile.php
@@ -27,20 +27,20 @@ $stmt->close();
 <html>
 	<head>
 		<meta charset="utf-8">
-		<title>Profile Page</title>
+		<title>Profiel</title>
 		<link href="style.css" rel="stylesheet" type="text/css">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
 	</head>
 	<body class="loggedin">
 		<nav class="navtop">
 			<div>
-				<h1>Website Title</h1>
+				<h1>Logistiek Memory</h1>
 				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
 				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
 			</div>
 		</nav>
 		<div class="content">
-			<h2>Profile Page</h2>
+			<h2>Profiel pagina</h2>
 			<div>
 				<p>Uw account informatie:</p>
 				<table>
