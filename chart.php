1ec1d9d9e78e423a7b356decf6e7f34b7a4659ce
RiverZdrojewski <56383094+RiverZdrojewski@users.noreply.github.com>
2021-04-22 16:13:50 +0200

    22-4 chart

--- chart.php
+++ chart.php
@@ -1,53 +1,24 @@
-  <?php
+
+<html>
+<body>
+<table>
+<tr> 
+<th>User Id </th>
+<th>Score </th>
+<th>Tijd  </th>
+</tr>
+<h1>Bar grafiek met de hoogste score</h1>
+<?php
   $con = mysqli_connect("localhost","root","","db_unity");
+  $sql = 'SELECT * FROM tb_dat';
+  $result = $con-> querry($sql);
   if($con){
     echo("connected");
   }
+if ($result->num_rows > 0) {
+  while ($row = $result-> fetch_assoc()) {
+    echo "<tr><td>" . $row["id"] . "</td><td>" . $row["score"] . "</td><td>" . $row["Time"] . "</td>"
+  }
+}
 ?>
-<html>
-  <head>
-  <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-  <script type="text/javascript">
-  google.charts.load('current', {'packages':['corechart', 'bar']});
-  google.charts.setOnLoadCallback(drawChart);
-  
-  function drawChart() {
-        var data = google.visualization.arrayToDataTable['id', 'score','time']    
-        };
-        </script>
-<?php
-                        if ($result = $con -> query("SELECT * FROM tb_dat")) {
-                          echo "Returned rows are: " . $result -> num_rows;
-                          // Free result set
-                          $result -> free_result();
-                          
-                        }   
-                        
-                        
-
-                    ?>  
-                    <script>
-                    var options = {
-          chart: {
-            title: 'Score Leerlingen',
-            subtitle: '' 
-          },
-          bars: 'horizontal' // Required for Material Bar Charts.
-        };
-        
-        var chart = new google.charts.Bar(document.getElementById('barchart_material'));
-
-         chart.draw(data, google.charts.Bar.convertOptions(options));
-         </script>
-<div class="row">
-  <div class="col-md-12" style="padding-left:3px; padding-right:3px">
-    <div id="chart_div" style="width: 100%; height: 420px;"></div>
-  </div>
-</div>    
-
-<div class="row">
-  <div class="col-md-4" style="margin-left:10px; padding-left:3px; padding-right:3px">
-    <div id="barchart_material" style="width: 900px; height: 500px;"></div>
-  </div>    
-</div>
 </html>
